bb0e01e14a581c5775bf7e495e0cb3b4d64feff4
Evan Graff <evangraff03@gmail.com>
2024-09-02 13:32:58 -0400

    API for login

---
+++ login.php
@@ -0,0 +1,72 @@
+<?php
+
+	session_start();
+	
+	header('Content-type: application/json');
+	
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+	
+	$database_url = parse_url(getenv('DATABASE_URL'));
+
+	$server = $database_url['host'];
+	$user = $database_url['user'];
+	$pass = $database_url['pass'];
+	$db = substr($database_url['path'], 1);
+	
+	$conn = new mysqli($server, $user, $pass, $db);
+	if($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		
+		if ($row = $result->fetch_assoc())
+		{
+			$_SESSION["id"] = $row['ID'];
+			$_SESSION["firstName"] = $row['firstName'];
+			$_SESSION["lastName"] = $row['lastName'];
+			
+			returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
