b17fa0ca92821126873de2c3e3d8a4af28690b90
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-09-08 18:22:26 -0400

    small fixes

--- public/api/deleteContact.php
+++ public/api/deleteContact.php
@@ -1,65 +1,96 @@
 <?php
 
-    session_start();
-  
-    $inData = getRequestInfo();
-  
-  	$database_url = parse_url(getenv('DATABASE_URL'));
-  
-  	$server = $database_url['host'];
-    $username = $database_url['user'];
-    $password = $database_url['pass'];
-    $db = substr($database_url['path'], 1);
-  
-  
-    $conn = new mysqli($server, $username, $password, $db);
-  	{
-  		returnWithError( $conn->connect_error );
-  	} 
-  	else
-  	{
-   
-  		$stmt = $conn->prepare("DELETE FROM contacts WHERE id = ? AND user_id = ?");        
-       
-      if ($stmt === false) {
-          returnWithError("Prepare failed: " . $conn->error);
-      }
-                    
-  		$stmt->bind_param("ss", $inData["id"], $_SESSION['id']);
-    
-      if ($stmt->execute()) {
-        returnWithInfo("deleted successfully!");
-      } else {
-        returnWithError("Deletion failed: " . $stmt->error);
-      }
-      
-    		$stmt->close();
-  		  $conn->close();
-    	
-  	}
-  
-  	function getRequestInfo()
-  	{
-  		return json_decode(file_get_contents('php://input'), true);
-  	}
-  
-  	function sendResultInfoAsJson( $obj )
-  	{
-  		header('Content-type: application/json');
-  		echo $obj;
-  	}
-  	
-  	function returnWithError( $err )
-  	{
-  		$retValue = json_encode(["id" => 0, "firstName" => "", "lastName" => "","phone_number" => "","email" => "", "error" => $err]);
-  		sendResultInfoAsJson( $retValue );
-  	}
-  	
-   function returnWithInfo($message)
-    {
-      $retValue = json_encode(["message" => $message, "error" => ""]);
-      sendResultInfoAsJson($retValue);
-    }
-  
-?>
+// If the session is active
+if (session_status() == PHP_SESSION_ACTIVE) {
+        $inData = getRequestInfo();
 
+        $keys_exist = true;
+        $keys_exist = $keys_exist and array_key_exists("contact_id", $inData);
+
+        if ($keys_exist) {
+                $database_url = parse_url(getenv('DATABASE_URL'));
+
+                $server = $database_url['host'];
+                $username = $database_url['user'];
+                $password = $database_url['pass'];
+                $db = substr($database_url['path'], 1);
+
+                $conn = new mysqli($server, $username, $password, $db);
+                if ($conn->connect_errno) {
+                        sendInternalError();
+                } else {
+                        $cnct_exists = checkContactExists($conn, $inData["contact_id"]);
+
+                        if ($cnct_exists) {
+                                $stmt = $conn->prepare("DELETE FROM contacts WHERE id = ? AND user_id = ?");
+
+                                if ($stmt == false) {
+                                        sendInternalError();
+                                } else {
+                                        $stmt->bind_param("ss", $inData["contact_id"], $_SESSION['id']);
+
+                                        if ($stmt->execute()) {
+                                                sendResponse(200, "success", "");
+                                        } else {
+                                                sendInternalError();
+                                        }
+
+                                        $stmt->close();
+                                        $conn->close();
+                                }
+                        } else {
+                                sendResponse(404, "contact_id_not_found", "the supplied contact id does not exist");
+                        }
+                }
+        } else {
+                sendResponse(400, "bad_request", "missing required api parameters");
+        }
+} else {
+        sendResponse(401, "unauthorized", "user is not logged in");
+}
+
+function checkContactExists($conn, $contact_id)
+{
+        $stmt = $conn->prepare("SELECT EXISTS(SELECT 1 FROM contacts WHERE id=? AND user_id=?)");
+        if ($stmt == false) {
+                sendInternalError();
+        } else {
+                $stmt->bind_param("ss", $contact_id, $_SESSION["id"]);
+                if ($stmt->execute()) {
+                        if ($stmt->num_rows > 0) {
+                                return true;
+                        } else {
+                                return false;
+                        }
+                } else {
+                        sendInternalError();
+                }
+        }
+        $stmt->close();
+        return false;
+}
+
+function getRequestInfo()
+{
+        return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResponse($http_code, $status, $desc)
+{
+        $res = '{"status": "' . $status . '", "desc": "' . $desc . '"}';
+        http_response_code($http_code);
+        sendResultInfoAsJson($res);
+}
+
+function sendInternalError()
+{
+        $res = '{"status": "error", "desc": "internal error"}';
+        http_response_code(500);
+        sendResultInfoAsJson($res);
+}
+
+function sendResultInfoAsJson($obj)
+{
+        header('Content-type: application/json');
+        echo $obj;
+}
--- public/api/editContact.php
+++ public/api/editContact.php
@@ -1,65 +1,99 @@
 <?php
 
-    session_start();
-  
-    $inData = getRequestInfo();
-  
-  	$database_url = parse_url(getenv('DATABASE_URL'));
-  
-  	$server = $database_url['host'];
-    $username = $database_url['user'];
-    $password = $database_url['pass'];
-    $db = substr($database_url['path'], 1);
-  
-  
-    $conn = new mysqli($server, $username, $password, $db);
-  	{
-  		returnWithError( $conn->connect_error );
-  	} 
-  	else
-  	{
-   
-  		$stmt = $conn->prepare("UPDATE contacts SET first_name = ?, last_name = ?, phone_number = ?, email = ? WHERE id = ? AND user_id = ?;");  
-             
-      if ($stmt === false) {
-          returnWithError("Prepare failed: " . $conn->error);
-      }
-                    
-  		$stmt->bind_param("ssssss", $inData["first_name"], $inData["last_name"], $inData["phone_number"], $inData["email"], $inData["id"], $_SESSION['id']);
-    
-      if ($stmt->execute()) {
-        returnWithInfo("Updated successfully!");
-      } else {
-        returnWithError("Update failed: " . $stmt->error);
-      }
-      
-    		$stmt->close();
-  		  $conn->close();
-     
-  	}
-  
-  	function getRequestInfo()
-  	{
-  		return json_decode(file_get_contents('php://input'), true);
-  	}
-  
-  	function sendResultInfoAsJson( $obj )
-  	{
-  		header('Content-type: application/json');
-  		echo $obj;
-  	}
-  	
-  	function returnWithError( $err )
-  	{
-  		$retValue = json_encode(["id" => 0, "firstName" => "", "lastName" => "","phone_number" => "","email" => "", "error" => $err]);
-  		sendResultInfoAsJson( $retValue );
-  	}
-  	
-   function returnWithInfo($message)
-    {
-      $retValue = json_encode(["message" => $message, "error" => ""]);
-      sendResultInfoAsJson($retValue);
-    }
-  
-?>
+if (session_status() == PHP_SESSION_ACTIVE) {
+        $inData = getRequestInfo();
 
+        $keys_exist = true;
+        $keys_exist = $keys_exist and array_key_exists("first_name", $inData);
+        $keys_exist = $keys_exist and array_key_exists("last_name", $inData);
+        $keys_exist = $keys_exist and array_key_exists("phone_number", $inData);
+        $keys_exist = $keys_exist and array_key_exists("email", $inData);
+        $keys_exist = $keys_exist and array_key_exists("contact_id", $inData);
+
+        if ($keys_exist) {
+                $database_url = parse_url(getenv('DATABASE_URL'));
+
+                $server = $database_url['host'];
+                $username = $database_url['user'];
+                $password = $database_url['pass'];
+                $db = substr($database_url['path'], 1);
+
+                $conn = new mysqli($server, $username, $password, $db);
+                if ($conn->connect_errno) {
+                        sendInternalError();
+                } else {
+                        $cnct_exists = checkContactExists($conn, $inData["contact_id"]);
+
+                        if ($cnct_exists) {
+                                $stmt = $conn->prepare("UPDATE contacts SET first_name = ?, last_name = ?, phone_number = ?, email = ? WHERE id = ? AND user_id = ?;");
+
+                                if ($stmt == false) {
+                                        sendInternalError();
+                                } else {
+                                        $stmt->bind_param("ssssss", $inData["first_name"], $inData["last_name"], $inData["phone_number"], $inData["email"], $inData["contact_id"], $_SESSION['id']);
+
+                                        if ($stmt->execute()) {
+                                                sendResponse(200, "success", "");
+                                        } else {
+                                                sendInternalError();
+                                        }
+
+                                        $stmt->close();
+                                        $conn->close();
+                                }
+                        } else {
+                                sendResponse(404, "contact_id_not_found", "the supplied contact id does not exist");
+                        }
+                }
+        } else {
+                sendResponse(400, "bad_request", "missing required api parameters");
+        }
+} else {
+        sendResponse(401, "unauthorized", "user is not logged in");
+}
+
+function checkContactExists($conn, $contact_id)
+{
+        $stmt = $conn->prepare("SELECT EXISTS(SELECT 1 FROM contacts WHERE id=? AND user_id=?)");
+        if ($stmt == false) {
+                sendInternalError();
+        } else {
+                $stmt->bind_param("ss", $contact_id, $_SESSION["id"]);
+                if ($stmt->execute()) {
+                        if ($stmt->num_rows > 0) {
+                                return true;
+                        } else {
+                                return false;
+                        }
+                } else {
+                        sendInternalError();
+                }
+        }
+        $stmt->close();
+        return false;
+}
+
+function getRequestInfo()
+{
+        return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+        header('Content-type: application/json');
+        echo $obj;
+}
+
+function sendResponse($http_code, $status, $desc)
+{
+        $res = '{"status": "' . $status . '", "desc": "' . $desc . '"}';
+        http_response_code($http_code);
+        sendResultInfoAsJson($res);
+}
+
+function sendInternalError()
+{
+        $res = '{"status": "error", "desc": "internal error"}';
+        http_response_code(500);
+        sendResultInfoAsJson($res);
+}
