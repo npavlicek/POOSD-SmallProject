3acb01909f854629b9befedb8a7dac7123795116
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-09-11 20:27:34 -0400

    updated api

---
+++ public/register.php
@@ -0,0 +1,50 @@
+<?php
+
+session_start();
+
+if (!$_SESSION['logged_in']) {
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <title>Contact App Registration</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="container mt-5">
+            <form onsubmit="return doRegister();">
+                <div class="mb-3">
+                    <label for="first_name" class="form-label">First Name</label>
+                    <input type="text" class="form-control" id="first_name">
+                </div>
+                <div class="mb-3">
+                    <label for="last_name" class="form-label">Last Name</label>
+                    <input type="text" class="form-control" id="last_name">
+                </div>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+                <button id="register_button" type="submit" class="btn btn-primary">Register</button>
+            </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="/js/main.js"></script>
+    </body>
+
+    </html>
+
+<?php
+} else {
+    header('Location: /');
+}
+?>
