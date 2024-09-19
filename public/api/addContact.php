d2bfaf37e1b8f329a32b5d27bdb14b475c782504
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-09-18 23:00:51 -0400

    small update

--- public/api/addContact.php
+++ public/api/addContact.php
@@ -1,58 +1,74 @@
 <?php
 
-$inData = getRequestInfo();
+session_start();
 
-$first = $inData["first name"];
-$last = $inData["last_name"];
-$phone = $inData["phone_number"];
-$email = $inData["email"];
-$user_id = $inData["user_id"];
-$id = $inData["id"];
+if ($_SESSION['logged_in']) {
+	$inData = getRequestInfo();
 
-$database_url = parse_url(getenv('DATABASE_URL'));
+	$first = $inData["first_name"];
+	$last = $inData["last_name"];
+	$phone = $inData["phone_number"];
+	$email = $inData["email"];
 
-$server = $database_url['host'];
-$user = $database_url['user'];
-$pass = $database_url['pass'];
-$db = substr($database_url['path'], 1);
+	$keys_exist = true;
+	$keys_exist = $keys_exist && isset($inData["first_name"]);
+	$keys_exist = $keys_exist && isset($inData["last_name"]);
+	$keys_exist = $keys_exist && isset($inData["phone_number"]);
+	$keys_exist = $keys_exist && isset($inData["email"]);
 
-$conn = new mysqli($server, $user, $pass, $db);
+	if ($keys_exist) {
+		$database_url = parse_url(getenv('DATABASE_URL'));
 
-if ($conn->connect_errno) 
-{
-	returnWithError("Trouble Connecting to database");
-} else 
-{
-	$stmt = conn->prepare("INSERT into contacts (first_name, last_name, phone_number, email, user_id, id) Values(?, ?, ?, ?, ?, ?)");
-	$stmt->bind_param("ssssss", $first, $last, $phone, $email, $user_id, $id);
-	if(!$stmt->execute())
-	{
-		returnWithError("unable to add contact");
-	} else
-	{
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
+		$server = $database_url['host'];
+		$user = $database_url['user'];
+		$pass = $database_url['pass'];
+		$db = substr($database_url['path'], 1);
+
+		$conn = new mysqli($server, $user, $pass, $db);
+
+		if ($conn->connect_errno) {
+			sendInternalError();
+		} else {
+			$stmt = $conn->prepare("INSERT INTO contacts (first_name, last_name, phone_number, email, user_id) VALUES (?, ?, ?, ?, ?)");
+			$stmt->bind_param("ss", $first, $last, $phone, $email, $_SESSION['id']);
+
+			if ($stmt->execute()) {
+				sendResponse(200, "success", "");
+			} else {
+				sendInternalError();
+			}
+
+			$stmt->close();
+			$conn->close();
+		}
+	} else {
+		sendResponse(400, "bad_request", "missing required api parameters");
 	}
+} else {
+	sendResponse(401, "unauthorized", "user is not logged in");
 }
-	
-	
-	
+
 function getRequestInfo()
 {
 	return json_decode(file_get_contents('php://input'), true);
 }
 
-function sendResultInfoAsJson( $obj )
+function sendResultInfoAsJson($obj)
 {
 	header('Content-type: application/json');
 	echo $obj;
 }
 
-function returnWithError($err)
+function sendResponse($http_code, $status, $desc)
 {
-	$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-	sendResultInfoAsJson($retValue);
+	$res = '{"status": "' . $status . '", "desc": "' . $desc . '"}';
+	http_response_code($http_code);
+	sendResultInfoAsJson($res);
 }
-?>
+
+function sendInternalError()
+{
+	$res = '{"status": "error", "desc": "internal error"}';
+	http_response_code(500);
+	sendResultInfoAsJson($res);
+}
--- public/api/searchContact.php
+++ public/api/searchContact.php
@@ -1,80 +1,136 @@
 <?php
 
-$inData = getRequestInfo();
-	
-$searchResults = "";
-$searchCount - 0;	
+session_start();
 
-$database_url = parse_url(getenv('DATABASE_URL'));
+/*
+* api endpoint:
+*
+* {
+*	"limit": number,
+*	"offset": number,
+*	"search_query": string, // NOT REQUIRED, if no query is present this endpoint will return every contact
+* }
+*/
 
-$server = $database_url['host'];
-$user = $database_url['user'];
-$pass = $database_url['pass'];
-$db = substr($database_url['path'], 1);
+if ($_SESSION['logged_in']) {
+	$inData = getRequestInfo();
 
-$conn = new mysqli($server, $user, $pass, $db);
-if($conn->connect_errno) 
-{
-	returnWithError("Trouble connecting to database");
-}
-else
-{
-	$stmt = $conn->prepare("select * from Contacts where (first_name like ? OR last_name like ?)  and user_id=?");
-	$name = "%" . $inData["search"] . "%";
-	stmt->bind_param("sss", $name, $name, $inData["user_id"]);
-	if(!stmt->execute())
-	{
-		returnWithError("Unable to complete search");
+	$keys_exist = true;
+	$keys_exist = $keys_exist && isset($inData["limit"]);
+	$keys_exist = $keys_exist && isset($inData["offset"]);
+
+	$search_query = null;
+	if (isset($inData["search_query"])) {
+		$search_query = $inData["search_query"];
+	} else {
+		$search_query = "%";
 	}
-	else
-	{
-		$stmt->execute();
-		$result = $stmt->get_result();
-		
-		while($row = $result->fetch_assoc())
-		{
-			if($searchCount > 0)
-			{
-				$searchResults .= ",";
+
+	if ($keys_exist) {
+		$database_url = parse_url(getenv('DATABASE_URL'));
+
+		$server = $database_url['host'];
+		$user = $database_url['user'];
+		$pass = $database_url['pass'];
+		$db = substr($database_url['path'], 1);
+
+		$conn = new mysqli($server, $user, $pass, $db);
+		if ($conn->connect_errno) {
+			sendInternalError();
+		} else {
+			$query = "SELECT * FROM contacts ";
+
+			if (isset($inData["search_query"])) {
+				$query = $query . "WHERE (first_name LIKE CONCAT('%', ?, '%') OR last_name LIKE CONCAT('%', ?, '%') 
+				OR phone_number LIKE CONCAT('%', ?, '%') OR email LIKE CONCAT('%', ?, '%')) 
+				AND user_id=?";
+			} else {
+				$query = $query . "WHERE user_id=?";
 			}
-			$searchCount++;
-			$searchResults .= '{"first name" : "' . $row["first_name"]. '", "last name" : "' . $row[last_name]. '", "Phone" : "' . $row["phone_number"]. '", "Email" : "' . $row["email"] . '"}';
-		}
-		
-		if($searchCount == 0)
-		{
-			returnWithError("No Records Found");
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
-	}
-	$stmt->close();
-	$conn->close();
-}
 
+			$query = $query . " ORDER BY id DESC LIMIT ? OFFSET ?;";
+
+			$stmt = $conn->prepare($query);
+
+			if (isset($inData["search_query"])) {
+				$stmt->bind_param(
+					"ssssiii",
+					$inData['search_query'],
+					$inData['search_query'],
+					$inData['search_query'],
+					$inData['search_query'],
+					$_SESSION['id'],
+					$inData['limit'],
+					$inData['offset']
+				);
+			} else {
+				$stmt->bind_param(
+					"iii",
+					$_SESSION['id'],
+					$inData['limit'],
+					$inData['offset']
+				);
+			}
+
+			if ($stmt->execute()) {
+				$res = $stmt->get_result();
+
+				if ($res->num_rows > 0) {
+					$res_json = "{ \"results\": [";
+
+					while ($cur_field = $res->fetch_assoc()) {
+						$res_json = $res_json . "{";
+
+						$res_json = $res_json . "\"first_name\": \"" . $cur_field["first_name"] . "\",";
+						$res_json = $res_json . "\"last_name\": \"" . $cur_field["last_name"] . "\",";
+						$res_json = $res_json . "\"phone_number\": \"" . $cur_field["phone_number"] . "\",";
+						$res_json = $res_json . "\"email\": \"" . $cur_field["email"] . "\",";
+						$res_json = $res_json . "\"id\": \"" . $cur_field["id"] . "\",";
+
+						$res_json = $res_json . "},";
+					}
+
+					$res_json = $res_json . "]}";
+
+					sendResultInfoAsJson($res_json);
+				} else {
+					sendResultInfoAsJson("{}");
+				}
+			} else {
+				sendInternalError();
+			}
+
+			$stmt->close();
+			$conn->close();
+		}
+	} else {
+		sendResponse(400, "bad_request", "missing required api parameters");
+	}
+} else {
+	sendResponse(401, "unauthorized", "user is not logged in");
+}
 
 function getRequestInfo()
 {
 	return json_decode(file_get_contents('php://input'), true);
 }
 
-function sendResultInfoAsJson( $obj )
+function sendResultInfoAsJson($obj)
 {
-		header('Content-type: application/json');
-		echo $obj;
+	header('Content-type: application/json');
+	echo $obj;
 }
 
-function returnWithError( $err )
+function sendResponse($http_code, $status, $desc)
 {
-		$retValue = '{"id":0,"first_name":"","last_name":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+	$res = '{"status": "' . $status . '", "desc": "' . $desc . '"}';
+	http_response_code($http_code);
+	sendResultInfoAsJson($res);
 }
 
-function returnWithInfo( $searchResults )
+function sendInternalError()
 {
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
+	$res = '{"status": "error", "desc": "internal error"}';
+	http_response_code(500);
+	sendResultInfoAsJson($res);
 }
-?>
