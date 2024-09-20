23ae0d5f5fb4396654b4bce2e38645b8c14543e5
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-09-20 19:06:07 -0400

    implementing initContacts functionality

--- public/contacts.php
+++ public/contacts.php
@@ -1,9 +1,9 @@
 <?php
+
 session_start();
 
 if ($_SESSION['logged_in']) {
 ?>
-
     <!DOCTYPE html>
     <html lang="en">
 
@@ -13,57 +13,30 @@ if ($_SESSION['logged_in']) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="css/styles.css">
-        <style>
-            
-        </style>
     </head>
 
     <body>
+        <div class="container mt-5">
+            <?php
+            echo '<h1 class="pt-5"><b>Welcome ' . $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] . '</b>!</h1>';
+            ?>
 
-    <div class="container">
-        <?php
-        echo '<h1 class="pt-5"><b>Welcome ' . $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] . '</b>!</h1>';
-        ?>
-        <a href="/logout.php" class="mt-3">Logout</a>
-    </div>
+            <a href="/logout.php" class="mt-3">Logout</a>
+            <h1 class="text-center mb-5">Contact List</h1>
 
-    
+            <div>
+                <button class="new-button-contact" onclick=""> + Create New Contact</button>
+            </div>
 
-    <!-- ChatGPT helped me create the boxes -->
-    <div class="container mt-5">
-        <h1 class="text-center mb-5">Contact List</h1>
-        
-        <div>
-            <button class="new-button-contact" onclick=""> + Create New Contact</button>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <div class="card contact-card">
-                    <div class="card-body">
-                        <h5 class="card-title"> Andrew Victor</h5>
-                        <p class="card-text increase-top-gap reduce-bottom-gap">xyz@gmail.com</p>
-                        <p class="card-text">786-928-9472</p>
-                        <div class="image-link">
-                            <!-- Delete button -->
-                            <img src="https://pngimg.com/d/trash_can_PNG18460.png" class="red-background" alt="Trash Can" onclick="confirmDelete()">
-                            <!-- Edit button -->
-                            <img src="https://www.clipartmax.com/png/small/351-3513671_revise-icon-transparent-background-editing-icon.png" class="blue-background" alt="Edit">
-                        </div>
-                    </div>
-                </div>
+            <div id="contacts-container" class="row row-cols-3">
             </div>
         </div>
-        
-    </div>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="/js/main.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="/js/main.js"></script>
     </body>
+
     </html>
-
-
 <?php
 } else {
     header('Location: /');
--- public/js/main.js
+++ public/js/main.js
@@ -140,3 +140,92 @@ function confirmDelete() {
 		alert("It works");
 	}
 }
+
+function appendContact(contact) {
+	let node = document.getElementById("contacts-container");
+
+	let columnNode = document.createElement("div");
+	columnNode.classList.add('col');
+
+	let contactCard = document.createElement("div");
+	contactCard.classList.add('card', 'mb-3');
+	contactCard.setAttribute('data-contact-id', contact.id);
+
+	let cardHeader = document.createElement("h5");
+	cardHeader.classList.add('card-header');
+	cardHeader.innerText = contact.first_name + " " + contact.last_name;
+
+	let cardFooter = document.createElement("div");
+	cardFooter.classList.add('card-footer', 'text-body-secondary', 'text-end');
+	cardFooter.innerText = "Created: " + contact.date_created;
+
+	let emailItem = document.createElement("li");
+	emailItem.classList.add('list-group-item');
+	emailItem.innerText = 'Email: ' + contact.email;
+
+	let phoneNumberItem = document.createElement("li");
+	phoneNumberItem.classList.add('list-group-item');
+	phoneNumberItem.innerText = 'Phone: ' + contact.phone_number;
+
+	let listGroup = document.createElement("ul");
+	listGroup.classList.add('list-group', 'list-group-flush');
+
+	listGroup.appendChild(emailItem);
+	listGroup.appendChild(phoneNumberItem);
+
+	contactCard.appendChild(cardHeader);
+	contactCard.appendChild(listGroup);
+	contactCard.appendChild(cardFooter);
+
+	columnNode.appendChild(contactCard);
+
+	node.appendChild(columnNode);
+}
+
+let currentContactOffset = 0;
+
+function initContacts() {
+	const reqBody = JSON.stringify({
+		limit: 15,
+		offset: 0
+	});
+
+	currentContactOffset = 15;
+	
+	fetch(
+		'./api/searchContact.php',
+		{
+			method: 'post',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: reqBody,
+			credentials: 'include'
+		}
+	).then(response => {
+		return response.json();
+	}).then(json => {
+		if (json.status === 'success') {
+			for (contact of json.results) {
+				appendContact(contact);
+			}
+		}
+	}).catch(function(error) {
+		console.log(error);
+	});
+}
+
+initContacts();
+
+let scrollTimeout = false;
+
+window.onscroll = function(ev) {
+	if (!scrollTimeout) {
+		scrollTimeout = true;
+		setTimeout(_ => {scrollTimeout = false;}, 400);
+		if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 10) {
+			console.log("document height: " + document.body.scrollHeight);
+			console.log("current y: " + (window.innerHeight + window.scrollY));
+		}
+	}
+}
