ee4dfcdb2bb8d201f40376853fd67407cbc2e426
austinchenoweth <austindavidchenoweth@gmail.com>
2024-09-25 16:23:34 -0400

    poorly added edit for front end?

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -324,3 +324,190 @@ function createAddContactInputCard() {
 
 	return columnNode;
 }
+
+
+function editContactCard(contact_id)
+{
+	let original = document.getElementById("contact-card-" + contact_id);
+
+	let json = getContact(contact_id);
+
+	let columnNode = document.createElement("div");
+	columnNode.classList.add('col');
+
+	let contactCard = document.createElement("div");
+	contactCard.classList.add('card', 'mb-3');
+
+	let cardHeader = document.createElement("div");
+	cardHeader.classList.add('card-header');
+
+	// cardHeader.innerHTML = `
+	// 	<div class="d-flex align-items-center">
+	// 		<h5 class="flex-grow-1 m-0">Edit Contact</h5>
+	// 		<button class="btn btn-primary me-1" onclick="editContactSubmit()">
+	// 			<i class="bi bi-check-lg"></i>
+	// 		</button>
+	// 		<button class="btn btn-danger" onclick="editContactCancel()">
+	// 			<i class="bi bi-x-lg"></i>
+	// 		</button>
+	// 	</div>`;
+	//  Below essentially does the whats above
+	let div = document.createElement("div");
+	div.setAttribute("class", "d-flex align-items-center");
+	let h5 = document.createElement("h5");
+	h5.setAttribute("class", "flex-grow-1 m-0");
+	h5.innerText = "Edit Contact";
+
+	let editSubmit = document.createElement("button");
+	editSubmit.setAttribute("class", "btn btn-primary me-1");
+	editSubmit.setAttribute("onclick", "editContactSubmit(" + contact_id + ")");
+
+	let isubmit = document.createElement("i");
+	isubmit.setAttribute("class", "bi bi-check-lg");
+
+	let editCancel = document.createElement("button");
+	editCancel.setAttribute("class", "btn btn-primary me-1");
+	editCancel.setAttribute("onclick", "editContactCancel(" + contact_id + ")");
+
+	let icancel = document.createElement("i");
+	icancel.setAttribute("class", "bi bi-check-lg");
+
+	editSubmit.appendChild(isubmit);
+	editCancel.appendChild(icancel);
+
+	div.appendChild(h5);
+	div.appendChild(editSubmit);
+	div.appendChild(editCancel);
+
+	cardHeader.appendChild(div);
+	// End
+
+
+	let firstNameItem = document.createElement("li");
+	firstNameItem.classList.add('list-group-item');
+
+	let firstNameInput = document.createElement("input");
+	firstNameInput.classList.add("form-control");
+	firstNameInput.value = json.first_name;
+	firstNameInput.id = "edit-contact-first-name-" + contact_id;
+	firstNameItem.appendChild(firstNameInput);
+
+	let lastNameItem = document.createElement("li");
+	lastNameItem.classList.add('list-group-item');
+
+	let lastNameInput = document.createElement("input");
+	lastNameInput.classList.add("form-control");
+	lastNameInput.value = json.last_name;
+	lastNameInput.id = "edit-contact-last-name-" + contact_id;
+	lastNameItem.appendChild(lastNameInput);
+
+	let emailItem = document.createElement("li");
+	emailItem.classList.add('list-group-item');
+
+	let emailInput = document.createElement("input");
+	emailInput.classList.add("form-control");
+	emailInput.value = json.email;
+	emailInput.id = "edit-contact-email-" + contact_id;
+	emailItem.appendChild(emailInput);
+
+	let phoneNumberItem = document.createElement("li");
+	phoneNumberItem.classList.add('list-group-item');
+
+	let phoneNumberInput = document.createElement("input");
+	phoneNumberInput.classList.add("form-control");
+	phoneNumberInput.value = json.phone_number;
+	phoneNumberInput.id = "edit-contact-phone-number-" + contact_id;
+	phoneNumberItem.appendChild(phoneNumberInput);
+
+	let listGroup = document.createElement("ul");
+	listGroup.classList.add('list-group', 'list-group-flush');
+
+	listGroup.appendChild(firstNameItem);
+	listGroup.appendChild(lastNameItem);
+	listGroup.appendChild(emailItem);
+	listGroup.appendChild(phoneNumberItem);
+
+	contactCard.appendChild(cardHeader);
+	contactCard.appendChild(listGroup);
+
+	columnNode.appendChild(contactCard);
+	original.replaceWith(columnNode);
+}
+
+function editContactSubmit(contact_id)
+{
+	const first_name = document.getElementById("edit-contact-first-name-"  + contact_id);
+	const last_name = document.getElementById("edit-contact-last-name-"  + contact_id);
+	const phone_number = document.getElementById("edit-contact-phone-number-"  + contact_id);
+	const email = document.getElementById("edit-contact-email-"  + contact_id);
+
+	const reqBody = JSON.stringify({ 
+		first_name,
+		last_name,
+		phone_number,
+		email,
+		contact_id
+	 });
+
+	fetch(
+		'./api/editContact.php',
+		{
+			method: 'post',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: reqBody,
+			credentials: 'include'
+		}
+	).then(response => {
+		return response.json();
+	}).then(json => {
+		if (json.status === 'success') {
+			replaceContact(JSON.parse(reqBody));
+		}
+	}).catch(function(error) {
+		console.log(error);
+	});		
+}
+
+function editContactCancel(contact_id)
+{
+	const json = getContact(contact_id);
+	if(json == null) return;
+	replaceContact(json);
+}
+
+function replaceContact(json)
+{
+	let contactCard = createContactCard(json);
+	let oldElement= document.getElementById("contact-card-" + json.contact_id);
+	if(oldElement == null) return;
+	oldElement.replaceWith(contactCard);
+}
+
+function getContact(contact_id)
+{
+	//Get Input to Give to Contact
+	const reqBody = JSON.stringify({ contact_id });
+
+	fetch(
+		'./api/getContact.php',
+		{
+			method: 'post',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: reqBody,
+			credentials: 'include'
+		}
+	).then(response => {
+		return response.json();
+	}).then(json => {
+		if (json.status === 'success') {
+			// Will this Work?
+			return (json);
+		}
+	}).catch(function(error) {
+		console.log(error);
+	});		
+}
