016a43d8c111920547e10fe2505c5ff7b09028b7
Triple-A17 <andresabrams17@gmail.com>
2024-09-28 12:54:35 -0400

    Update contacts.php

--- public/contacts.php
+++ public/contacts.php
@@ -20,11 +20,11 @@ if ($_SESSION['logged_in']) {
         <div class="container-fluid">
             <div class="row vh-100">
                 <!-- change color later on of the block on the left below in the class card :)  -->
-            <div class="card p-3 col-2 mt-2"> 
+            <div class="card p-3 col-2 mt-2" style="background-color: rgba(255,255,255,0.5)"> 
                     <?php
                     echo '<h3>Welcome <br><b>' . $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] . '</b>!</h3>';
                     ?>
-                    <div class="vstack gap-2 col-md mx-auto" style="background-color: rgba(255,255,255,0.5)">
+                    <div class="vstack gap-2 col-md mx-auto">
                         <a href="/logout.php" class="btn btn-danger mt-3 rounded-3">Logout</a>
                         <button id="create-contact-button" class="btn btn-primary rounded-3" onclick="addContactBegin();">Create Contact</button>
                         <input type="text" aria-label="Search for Contact" placeholder="Search Contacts..." class="form-control rounded-3" id="searchbar" oninput="searchUpdate()">
